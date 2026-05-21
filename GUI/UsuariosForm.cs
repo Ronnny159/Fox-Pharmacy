3061dce7aec0dc3dba5136fde097dbeb36a38808
JUAN MIGUEL DEVILLEROS PIMIENTA <jdevilleros@unicesar.edu.co>
2026-05-21 10:03:12 -0500

    Creacion de Forms bases

--- GUI/UsuariosForm.cs
+++ GUI/UsuariosForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class UsuariosForm : Form
     {
-        public Form1()
+        public UsuariosForm()
         {
             InitializeComponent();
         }
